2c2e08c1bb46516dc10a0c7a3cf778b45fd08b33
Brandon Barsotti <barsotti@vt.edu>
2017-02-08 00:40:47 -0500

    Create QueensChamber.java

---
+++ QueensChamber.java
@@ -0,0 +1,30 @@
+ //-------------------------------------------------------------------------
+ /**
+  *  The queens chamber is the goal of the bees.
+  *  If the bees reach this, they win.
+  *
+  *  @author Brandon Barsotti (barsotti)
+  *  @version (2016.03.29)
+  */
+ public class QueensChamber extends Actor
+ {
+     //~ Fields ................................................................
+ 
+ 
+ 
+     //~ Constructor ...........................................................
+ 
+     // ----------------------------------------------------------
+     /**
+      * Creates a new QueensChamber object.
+      */
+     public QueensChamber()
+     {
+         /*# Do any work to initialize your class here. */
+         //bee = getOneIntersectingObject(Bee.class);
+     }
+ 
+ 
+     //~ Methods ...............................................................
+     
+ }
